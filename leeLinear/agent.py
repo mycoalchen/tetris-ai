3cfed7c54cc25942e95f8b72d5836ba1a173cdf5
mycoalchen <chen.michael.1116@gmail.com>
2025-03-04 16:19:36 -0600

    first commit

---
+++ leeLinear/agent.py
@@ -0,0 +1,3 @@
+# Given board and piece, return all possible future boards after placing this piece
+
+# Given board, piece, holder, and queue, decide the best spot to place this piece or whether to hold it
