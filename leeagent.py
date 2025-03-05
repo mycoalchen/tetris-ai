fix off-by-one error in lee controller

leeagent.py
@@ -2,7 +2,6 @@ import gymnasium as gym
 from tetris_gymnasium.envs import Tetris
 from utils import getPossibleBoards, BOARD_HEIGHT, BOARD_WIDTH, getCurrentBoardAndPiece
 import numpy as np
-import time
 import cv2
 
 
@@ -55,11 +54,11 @@ class leeAgent:
             ],
         )
 
-    def getPieceDecision(self, board, currentPiece):
+    def getPieceDecision(self, current_board, current_piece, current_piece_left_x):
         """
         Returns the (rotation, horizontal translation) tuple maximizing the rating of the board after hard dropping currentPiece
         """
-        possibleBoards = getPossibleBoards(board, currentPiece)
+        possibleBoards = getPossibleBoards(current_board, current_piece, current_piece_left_x)
         best_decision = ()
         best_rating = -1000000
         for (r, x), (new_board, _) in possibleBoards.items():
@@ -74,20 +73,29 @@ if __name__ == "__main__":
     agent = leeAgent()
 
     J = 0
+    prev1000 = 0
     new_piece = True
     # for _ in range(1):
     observation, _ = env.reset()
-    env.render()
+    # env.render()
     terminated = False
     while not terminated:
-        env.render()
-        # idk why but rendering doesn't work unless the next line is uncommented
-        # _ = cv2.waitKey(100)
+        # idk why but rendering doesn't work unless the next block is uncommented
+        # env.render()
+        # a = None
+        # while a is None:
+        #     _ = cv2.waitKey(1)
+        #     if _ == ord(" "):
+        #         a = 1
         if new_piece:
-            current_board, current_piece = getCurrentBoardAndPiece(
-                observation["board"], observation["active_tetromino_mask"]
+            current_board, current_piece, current_piece_left_x = (
+                getCurrentBoardAndPiece(
+                    observation["board"], observation["active_tetromino_mask"]
+                )
             )
-            decision = agent.getPieceDecision(current_board, current_piece)
+            decision = agent.getPieceDecision(
+                current_board, current_piece, current_piece_left_x
+            )
             if decision == ():
                 break
             r, x = decision
@@ -110,7 +118,8 @@ if __name__ == "__main__":
                 x -= np.sign(x)
         observation, reward, terminated, truncated, info = env.step(action)
         J += reward
-        # if J % 1000 == 0:
-        # print(J)
+        if J > prev1000:
+            print(J)
+            prev1000 += 1000
         # print(reward, terminated, truncated, info)
     print("J(pi)=", J)

utils.py
@@ -31,39 +31,54 @@ def can_place(board, piece, x, y):
     return True
 
 
-def getPossibleBoards(board: np.array, piece: np.array) -> dict[tuple, np.array]:
+def getPossibleBoards(
+    board: np.array, piece: np.array, starting_x: int
+) -> dict[tuple, np.array]:
     """
     Return all possible future boards after placing this piece, with corresponding (rotation, horizontal translation) tuples, given that it starts at piece_x, piece_y.
     Returns dict mapping valid (rotation, horizontal translation) tuples to resulting future boards. Output is 0/1 board.
     Rotations are counterclockwise.
     """
     possible = {}
+    piece_size = len(piece)  # pieces are always square
     for r in range(-1, 3):
         rotated_piece = np.rot90(piece, r)
-        for x_shift in range(-7, 7):
+        for x_shift in range(-9, 9):
             # find the lowest position that this piece can be dropped with this x (if possible)
             # account for gravity – piece must fall by one for every x translation
             y = abs(x_shift)
-            if not can_place(board, rotated_piece, x_shift + 7, y):
+            if not can_place(board, rotated_piece, x_shift + starting_x, y):
                 continue
             y += 1
-            while can_place(board, rotated_piece, x_shift + 7, y):
+            while can_place(board, rotated_piece, x_shift + starting_x, y):
                 y += 1
             y -= 1
             new_board = board.copy()
-            for i in range(4):
-                for j in range(4):
+            for i in range(piece_size):
+                for j in range(piece_size):
                     if rotated_piece[i, j] != 0:
-                        new_board[y + i, x_shift + j + 7] = 9
+                        new_board[y + i, x_shift + j + starting_x] = 9
             # Save the new board configuration with key (rotation, x translation)
             possible[(r, x_shift)] = (new_board, rotated_piece)
     return possible
 
 
-def getCurrentBoardAndPiece(raw_board, active_mask):
+def getCurrentBoardAndPiece(raw_board: np.array, active_mask: np.array):
     """
-    Separates the board from the active tetromino
+    Separates the board from the active tetromino. Also returns leftmost x coordinate of active tetromino
     """
     current_board = raw_board - raw_board * active_mask
-    current_piece = raw_board[0:4, 7:11]
-    return current_board, current_piece
+    # find top left and bottom right corners of active mask
+    x = 4
+    while not active_mask[0, x]:
+        x += 1
+    left = x
+    while active_mask[0, x]:
+        x += 1
+    right = x
+    x -= 1
+    y = 0
+    while active_mask[y, x]:
+        y += 1
+    bottom = y
+    return current_board, raw_board[:bottom, left:right], left