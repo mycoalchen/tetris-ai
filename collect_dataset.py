e670918e453d751717282182040b3ef493b99748
mycoalchen <chen.michael.1116@gmail.com>
2025-03-10 10:45:37 -0400

    add dataset collection script

--- collect_dataset.py
+++ collect_dataset.py
@@ -7,7 +7,6 @@ from utils import (
     getBestDecision,
     readQueue,
     read4x4,
-    getStartingX,
 )
 import numpy as np
 import cv2
@@ -15,41 +14,26 @@ import time
 from lee import leeRating
 
 
-def testLinearBot(
+def collectSamples(
+    num_samples,
     ratingFunction,
-    lookahead=2,
+    lookahead=0,
     num_beams=5,
-    numTrials=10,
-    render=False,
-    jUpdates=True,
-    stepUpdates=False,
-    allow_swaps=True,
+    allow_swaps=False,
 ):
-
-    env = gym.make("tetris_gymnasium/Tetris", render_mode="human")
-
-    J = 0
-    for i in range(numTrials):
-        curr_J, prev_J1000 = 0, 0
-        steps, moves = 0, 0
-        t0 = time.time()
-        calc_time = 0
+    t0, samples_collected = time.time(), 0
+    env = gym.make("tetris_gymnasium/Tetris")
+    observations = {
+        k: [None for _ in range(num_samples)]
+        for k in ["board", "active_tetromino_mask", "queue", "holder"]
+    }
+    while samples_collected < num_samples:
+        step_num = 0
         new_piece, can_swap = True, allow_swaps
         observation, _ = env.reset()
         terminated = False
-        while not terminated:
-            steps += 1
-            # idk why but rendering doesn't work unless the next block is uncommented
-            if render:
-                env.render()
-                a = None
-                while a is None:
-                    _ = cv2.waitKey(1)
-                    if _ == ord(" "):
-                        a = 1
+        while not terminated and samples_collected < num_samples:
             if new_piece:
-                t1 = time.time()
-                moves += 1
                 current_board, active_piece, active_piece_starting_y = (
                     getCurrentBoardAndPiece(
                         observation["board"], observation["active_tetromino_mask"]
@@ -71,7 +55,6 @@ def testLinearBot(
                 if decision == ():
                     break
                 r, x = decision
-                calc_time += time.time() - t1
             match r, x:
                 case False, False:
                     action = 6
@@ -91,41 +74,33 @@ def testLinearBot(
                     action = 0 if x < 0 else 1
                     x -= np.sign(x)
             observation, reward, terminated, truncated, info = env.step(action)
-            curr_J += reward
-            if stepUpdates:
-                if steps % 100 == 0:
-                    dt = time.time() - t0
-                    print(
-                        f"{steps} steps completed in {dt:.2f}s; average {1000 * dt/steps:.2f}ms per step"
-                    )
-                    print(
-                        f"{moves} moves completed in {dt:.2f}s; average {1000 * dt/moves:.2f}ms per move"
-                    )
-                    print(
-                        f"{calc_time:.2f}s spent calculating moves ({100 * calc_time/dt:.2f}%)"
-                    )
-            if jUpdates:
-                if curr_J - curr_J % 1000 > prev_J1000:
-                    print(f"Reached J = {curr_J} in {(time.time() - t0):.2f}s")
-                    prev_J1000 = curr_J - curr_J % 1000
-        print(f"Trial {i} terminated with reward {curr_J}")
-        J += curr_J
-    print("J(pi)=", J / numTrials)
+            step_num += 1
+            # only collect samples after step_num > 100 to reduce bias towards earlier game states
+            if step_num > 100:
+                for k in observation:
+                    observations[k][samples_collected] = observation[k]
+                samples_collected += 1
+            if samples_collected > 0 and samples_collected % 5_000 == 0:
+                print(
+                    f"Collected {samples_collected} samples in {(time.time() - t0):.2f}s"
+                )
+    for k in observations:
+        observations[k] = np.stack(observations[k])
+    np.savez("states_data", **observations)
 
 
 if __name__ == "__main__":
-    lookahead = 1
+    lookahead = 0
     num_beams = 5
     allow_swaps = False
+    N = 500_000
     print("lookahead:", lookahead)
     print("num_beams:", num_beams)
     print("allow_swaps:", allow_swaps)
-    testLinearBot(
-        leeRating,
-        render=False,
-        jUpdates=True,
-        stepUpdates=False,
-        numTrials=5,
+    print("N:", N)
+    collectSamples(
+        num_samples=N,
+        ratingFunction=leeRating,
         lookahead=lookahead,
         num_beams=num_beams,
         allow_swaps=allow_swaps,
